35fefefde7cb73883ed277c1618a323596b8cab2
Lw <645739460@QQ.com>
2015-10-02 22:29:37 +0800

    Add key control.

--- 7-seg display counter/count_by_sec.c
+++ 7-seg display counter/count_by_sec.c
@@ -3,11 +3,18 @@
 
 #define uint unsigned int
 #define uchar unsigned char
+#define ushort unsigned short
+
 #define MS 10    	  // MS decides show time of each digital
 #define INITIAL_NUM 15536	    //the initial value of count 50ms: 65536 - 50000 = 15536
 #define TIME_OF_50MS 20
 
 uint count = 0;
+ushort run = 1;
+sbit k5 = P3 ^ 5;
+sbit k1 = P1 ^ 4;
+sbit k2 = P1 ^ 5;
+
 uchar code digit[] = {
 	0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07,  0x7f, 0x6f
 };
@@ -19,7 +26,7 @@ void counter() interrupt 1 {
 
 	if (++num == TIME_OF_50MS) {
 		num = 0;
-		if (count++ > 9999)
+		if (run && count++ > 9999)
 			count = 0;
 	}
 }
@@ -34,7 +41,7 @@ void init() {
 }
 
 void delay_ms(uint time) {
-	int i = 110;
+	uint i = 110;
 	for (; time > 0; time--)
 		for (; i > 0; i--);
 }
@@ -46,32 +53,67 @@ uchar next_status(uchar s) {
 	return s;
 }
 
-void main() {
-	uint delay = 0;
-	uchar pos = 1; 
+
+
+void scan_key() {
+	if (!k5) {
+		//delay_ms(5);
+		if (!k5) {
+			while (!k5);
+			count = 0;
+		}
+	}
+
+	if (!k1) {
+		//delay_ms(5);
+		if (!k1) {
+			/* uncomment it and program show nothing! */
+			//while (!k1);
+			run = 0;
+		}
+	}
+
+	if (!k2) {
+		//delay_ms(5);
+		if (!k2) {
+		/* uncomment it and program show nothing! */
+			//while (!k2);
+			run = 1;
+		}
+	}
+		
+}
+
+void display() {
+	static uchar pos = 1;
+	P1 = pos;
+	pos = next_status(pos);
+	switch (pos) {
+	case 1:
+		P0 = digit[count / 1000];
+		break;
+	case 2:
+		P0 = digit[count % 10];
+		break;
+	case 4:
+		P0 = digit[count / 10 % 10];
+		break;
+	case 8:
+		P0 = digit[count / 100 % 10];
+		break;
+	default:
+		break;
+	}	
+	delay_ms(MS);	
+	//avoid conflict
+	P0 = 0;		
+}
+
+void main() { 
 	init();
 
 	while(1) {
-		P1 = pos;
-		pos = next_status(pos);
-		switch (pos) {
-		case 1:
-			P0 = digit[count / 1000];
-			break;
-		case 2:
-			P0 = digit[count % 10];
-			break;
-		case 4:
-			P0 = digit[count / 10 % 10];
-			break;
-		case 8:
-			P0 = digit[count / 100 % 10];
-			break;
-		default:
-			break;
-		}
-		delay_ms(MS);
-		//avoid conflict
-		P0 = 0;			 
+		scan_key();	
+		display();
 	}
 }
